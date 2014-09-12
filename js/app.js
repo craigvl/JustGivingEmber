a08685d9ccab60a406d4af00686500ce5f3d7058
Craig Waite <craigvl@tpg.com.au>
2014-09-12 16:20:12 +1000

    updates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.resource("charity",{path:"/charities/:category"})
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+     return Ember.$.getJSON("http://localhost:3000/charity/categories",function (data) {
+         //alert(data[1].category);
+        });
+     }
 });
 
 App.IndexController = Ember.ArrayController.extend({
@@ -18,5 +20,13 @@ App.IndexController = Ember.ArrayController.extend({
         clickMe: function (){
             alert("Clicked");
         }
+    }
+});
+
+App.CharityRoute = Ember.Route.extend({
+    model: function(params) {
+        //alert(JSON.stringify(params));
+        return Ember.$.getJSON("http://localhost:3000/charity/" + params.category ,function (data) {
+        });
     }
 });
